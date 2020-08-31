8c76c42593cc73ed95d71ded0f53376038cafba9
Edwin Otten <edwin.otten@sogeti.com>
2020-08-31 16:26:09 +0200

    feat: replaces lots of code with two dependencies (angular-data-converter-component and sportlink-to-mailchimp-converter)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,114 +1,33 @@
-import { Component, ViewEncapsulation, ViewChild, AfterViewInit, NgZone } from '@angular/core';
-import { DataConverterConfigService } from './data-converter-config.service';
-import { isNullOrUndefined } from 'util';
-import { IDataConverterService } from './interfaces/idata-converter-service';
-import { MatVerticalStepper } from '@angular/material';
-import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { Component } from '@angular/core'
+import { DataConverterConfig } from 'angular-data-converter-component'
+import { SportlinkToMailchimpConverter } from 'sportlink-to-mailchimp-converter'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements AfterViewInit {
-  fileLocation = '';
-  sourceFileContents = '';
-  destinationFileContents = '';
-  progressMode: 'determinate' | 'indeterminate' | 'buffer' | 'query' = 'determinate';
-  progressValue = 4;
-  config: IDataConverterService;
+export class AppComponent {
 
-  sourceError: string;
-  currentFile: File;
+  config: DataConverterConfig = {
+    inputFileExtensions: ['.csv'],
+    converterService: SportlinkToMailchimpConverter,
+    labels: {
+      sourceStep: {
+        title: 'Sportlink source',
+        description: '',
+      },
+      previewStep: {
+        title: 'Result preview',
+        description: 'Check out the preview below before exporting the result.',
+      },
+      outputStep: {
+        title: 'Mailchimp output',
+        description: 'Import this into mailchimp',
+      },
 
-  columns: string[];
-  dataSource: any[];
-
-  @ViewChild('stepper', {static: false}) private stepper: MatVerticalStepper;
-
-  constructor(private ngZone: NgZone,
-              private dataConverterService: DataConverterConfigService) {
-    this.config = dataConverterService;
+      selectFile: 'Select CSV file',
+    },
   }
 
-  ngAfterViewInit(): void {
-    this.stepper.selectionChange.subscribe((event: StepperSelectionEvent) => {
-      this.progressValue = 4 + (48 * event.selectedIndex);
-    });
-  }
-
-  fileInputAccept(): string {
-    return this.dataConverterService.inputFileExtensions.join(',');
-  }
-
-  openFile(event) {
-    this.currentFile = null;
-    this.progressMode = 'buffer';
-    this.dataSource = null;
-    this.columns = null;
-
-    for (const file of event.target.files) {
-      this.processFile(file);
-      break; // we only support reading one file at a time
-    }
-  }
-
-  async processFile(file: File) {
-    try {
-      this.currentFile = file;
-      const data = await this.dataConverterService.convertFileToPreview(file);
-
-      if (!isNullOrUndefined(data)) {
-        this.sourceError = null;
-        this.progressMode = 'determinate';
-        this.showPreview(data.columns, data.rows);
-      }
-    } catch (e) {
-      this.currentFile = null;
-      this.progressMode = 'determinate';
-      this.sourceError = e;
-    }
-  }
-
-  showPreview(columns: string[], data: any[]) {
-    this.dataSource = data;
-    this.columns = columns;
-
-    this.ngZone.run(() => {
-      this.stepper.selected.completed = true;
-      this.stepper.selectedIndex = 1;
-    });
-  }
-
-  async downloadFile() {
-    const output = await this.dataConverterService.convertFileToOutput(this.currentFile);
-    this.openSaveFileDialog(output.data, output.filename, output.mimetype);
-  }
-
-  openSaveFileDialog(data: any, filename: string, mimetype: string) {
-
-    if (!data) { return; }
-
-    const blob = data.constructor !== Blob
-      ? new Blob([data], {type: mimetype || 'application/octet-stream'})
-      : data ;
-
-    if (navigator.msSaveBlob) {
-      navigator.msSaveBlob(blob, filename);
-      return;
-    }
-
-    const lnk = document.createElement('a');
-    const url = window.URL;
-
-    if (mimetype) {
-      lnk.type = mimetype;
-    }
-
-    lnk.download = filename || 'untitled';
-    lnk.href = url.createObjectURL(blob);
-    lnk.dispatchEvent(new MouseEvent('click'));
-    setTimeout(url.revokeObjectURL.bind(url, lnk.href));
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { TextFieldModule } from '@angular/cdk/text-field';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card'
+import { DataConverterModule } from 'angular-data-converter-component'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     MatCardModule,
-    MatDividerModule,
-    MatStepperModule,
-    MatFormFieldModule,
-    MatInputModule,
-    TextFieldModule,
-    MatProgressBarModule,
-    MatGridListModule,
-    MatTableModule,
-    MatIconModule,
-    MatButtonModule,
-    FormsModule,
-    ReactiveFormsModule
+    DataConverterModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
